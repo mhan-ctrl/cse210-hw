1dc771e6594eb37eb476d9bef22fa562d6320511
mhan-ctrl <mhansen11@byupathway.edu>
2026-06-11 14:50:03 -0600

    Finished

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment Mark = new Assignment("Mark", "Geology");
+        Console.WriteLine($"{Mark.GetSummary()}");
+
+        MathAssignment John = new MathAssignment("John", "Philosophy", "Section 10.2A", "Prompts 1,2 and 4");
+        Console.WriteLine(John.GetHomeworkList());
+
+        WritingAssignment Mary = new WritingAssignment("Mary", "History", "The Times and Trials of the Venerable Carpet");
+        Console.WriteLine(Mary.GetWritingInformation());
     }
 }
